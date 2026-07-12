6f1c3549764f7c3bfc3177195e8525beb31b19a2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:25 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "signalr_service_network_acls" {
-  description = "All signalr_service_network_acl resources"
-  value       = azurerm_signalr_service_network_acl.signalr_service_network_acls
-}
 output "signalr_service_network_acls_default_action" {
-  description = "List of default_action values across all signalr_service_network_acls"
-  value       = [for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : v.default_action]
+  description = "Map of default_action values across all signalr_service_network_acls, keyed the same as var.signalr_service_network_acls"
+  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.default_action }
 }
 output "signalr_service_network_acls_private_endpoint" {
-  description = "List of private_endpoint values across all signalr_service_network_acls"
-  value       = [for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : v.private_endpoint]
+  description = "Map of private_endpoint values across all signalr_service_network_acls, keyed the same as var.signalr_service_network_acls"
+  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.private_endpoint }
 }
 output "signalr_service_network_acls_public_network" {
-  description = "List of public_network values across all signalr_service_network_acls"
-  value       = [for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : v.public_network]
+  description = "Map of public_network values across all signalr_service_network_acls, keyed the same as var.signalr_service_network_acls"
+  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.public_network }
 }
 output "signalr_service_network_acls_signalr_service_id" {
-  description = "List of signalr_service_id values across all signalr_service_network_acls"
-  value       = [for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : v.signalr_service_id]
+  description = "Map of signalr_service_id values across all signalr_service_network_acls, keyed the same as var.signalr_service_network_acls"
+  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.signalr_service_id }
 }
 
