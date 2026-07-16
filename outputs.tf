9b0bcbdfdc444ca846bb8e4b4b979596eb0f6d4b
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:54 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "signalr_service_network_acls_id" {
   description = "Map of id values across all signalr_service_network_acls, keyed the same as var.signalr_service_network_acls"
-  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.id }
+  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "signalr_service_network_acls_default_action" {
   description = "Map of default_action values across all signalr_service_network_acls, keyed the same as var.signalr_service_network_acls"
-  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.default_action }
+  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.default_action if v.default_action != null && length(v.default_action) > 0 }
 }
 output "signalr_service_network_acls_private_endpoint" {
   description = "Map of private_endpoint values across all signalr_service_network_acls, keyed the same as var.signalr_service_network_acls"
-  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.private_endpoint }
+  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.private_endpoint if v.private_endpoint != null && length(v.private_endpoint) > 0 }
 }
 output "signalr_service_network_acls_public_network" {
   description = "Map of public_network values across all signalr_service_network_acls, keyed the same as var.signalr_service_network_acls"
-  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.public_network }
+  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.public_network if v.public_network != null && length(v.public_network) > 0 }
 }
 output "signalr_service_network_acls_signalr_service_id" {
   description = "Map of signalr_service_id values across all signalr_service_network_acls, keyed the same as var.signalr_service_network_acls"
-  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.signalr_service_id }
+  value       = { for k, v in azurerm_signalr_service_network_acl.signalr_service_network_acls : k => v.signalr_service_id if v.signalr_service_id != null && length(v.signalr_service_id) > 0 }
 }
 
